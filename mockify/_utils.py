63113547a89321ab9376adf3feb68f6b6ef35271
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2018-10-13 09:15:11 +0200

    add test for cardinality objects

--- mockify/_utils.py
+++ mockify/_utils.py
@@ -1,2 +1,11 @@
 def is_cardinality_object(obj):
     return hasattr(obj, "_actual")
+
+
+def format_call_count(count):
+    if count == 1:
+        return "once"
+    elif count == 2:
+        return "twice"
+    else:
+        return "{} times".format(count)
--- mockify/cardinality.py
+++ mockify/cardinality.py
@@ -1,3 +1,6 @@
+from ._utils import format_call_count
+
+
 class Base:
 
     def __init__(self):
@@ -6,12 +9,8 @@ class Base:
     def format_actual(self):
         if self._actual == 0:
             return "never called"
-        elif self._actual == 1:
-            return "called once"
-        elif self._actual == 2:
-            return "called twice"
         else:
-            return "called {} times".format(self._actual)
+            return "called {}".format(format_call_count(self._actual))
 
     def update(self):
         self._actual += 1
@@ -20,18 +19,16 @@ class Base:
 class Exactly(Base):
 
     def __init__(self, expected):
+        if expected < 0:
+            raise TypeError("value of 'expected' must be >= 0")
         super().__init__()
         self._expected = expected
 
     def format_expected(self):
         if self._expected == 0:
             return "to be never called"
-        elif self._expected == 1:
-            return "to be called once"
-        elif self._expected == 2:
-            return  "to be called twice"
         else:
-            return "to be called {} times".format(self._expected)
+            return "to be called {}".format(format_call_count(self._expected))
 
     def is_satisfied(self):
         return self._expected == self._actual
@@ -40,14 +37,16 @@ class Exactly(Base):
 class AtLeast(Base):
 
     def __init__(self, minimal):
+        if minimal < 0:
+            raise TypeError("value of 'minimal' must be >= 0")
         super().__init__()
         self._minimal = minimal
 
     def format_expected(self):
-        if self._minimal == 1:
-            return "to be called at least once"
+        if self._minimal == 0:
+            return "to be called zero or more times"
         else:
-            return "to be called at least twice"
+            return "to be called at least {}".format(format_call_count(self._minimal))
 
     def is_satisfied(self):
         return self._minimal <= self._actual
@@ -55,12 +54,20 @@ class AtLeast(Base):
 
 class AtMost(Base):
 
+    def __new__(cls, maximal):
+        if maximal < 0:
+            raise TypeError("value of 'maximal' must be >= 0")
+        elif maximal == 0:
+            return Exactly(maximal)
+        else:
+            return super().__new__(cls)
+
     def __init__(self, maximal):
         super().__init__()
         self._maximal = maximal
 
     def format_expected(self):
-        return "to be called at most twice"
+        return "to be called at most {}".format(format_call_count(self._maximal))
 
     def is_satisfied(self):
         return self._maximal >= self._actual
@@ -68,13 +75,26 @@ class AtMost(Base):
 
 class Between(Base):
 
+    def __new__(cls, minimal, maximal):
+        if minimal > maximal:
+            raise TypeError("value of 'minimal' must not be greater than 'maximal'")
+        elif minimal < 0:
+            raise TypeError("value of 'minimal' must be >= 0")
+        elif minimal == maximal:
+            return Exactly(maximal)
+        elif minimal == 0:
+            return AtMost(maximal)
+        else:
+            return super().__new__(cls)
+
     def __init__(self, minimal, maximal):
         super().__init__()
         self._minimal = minimal
         self._maximal = maximal
 
     def format_expected(self):
-        return "to be called at least twice but no more than 3 times"
+        return "to be called at least {} but no more than {}".\
+            format(format_call_count(self._minimal), format_call_count(self._maximal))
 
     def is_satisfied(self):
         return self._minimal <= self._actual <= self._maximal
