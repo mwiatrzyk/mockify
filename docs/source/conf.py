195188d00dd62d7924e04bf54b286073803715e4
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2019-01-01 19:51:39 +0100

    add description to sidebar

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -92,7 +92,9 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'description': 'Mocking library for Python inspired by Google Mock C++ mocking toolkit'
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
