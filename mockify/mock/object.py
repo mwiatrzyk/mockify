0b00a3024579f64f07608b0c359bd218fa541c43
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2019-01-14 17:32:53 +0100

    record property set expectation

--- mockify/mock/object.py
+++ mockify/mock/object.py
@@ -4,26 +4,51 @@ from mockify.mock.function import Function
 
 class Object:
 
-    def __init__(self, name, methods=None, registry=None):
+    class _Property:
+
+        def __init__(self, name, registry):
+            self._fset = Function(name + '.fset', registry=registry)
+
+        @property
+        def fset(self):
+            return self._fset
+
+    def __init__(self, name, methods=None, properties=None, registry=None):
         self._name = name
         self._registry = registry or Registry()
         self._methods = dict(self.__create_methods(methods))
+        self._properties = dict(self.__create_properties(properties))
 
     def __create_methods(self, names):
         for name in names:
             yield name, Function("{}.{}".format(self._name, name), self._registry)
+
+    def __create_properties(self, names):
+        for name in names:
+            yield name, self._Property("{}.{}".format(self._name, name), self._registry)
 
     def __getattr__(self, name):
         if name in self._methods:
             return self._methods[name]
         else:
             raise AttributeError(
-                "{!r} mock object has no attribute named {!r}".
+                "mock object {!r} has no attribute named {!r}".
                 format(self._name, name))
+
+    def __setattr__(self, name, value):
+        if name.startswith('_'):
+            super().__setattr__(name, value)
+        elif name in self._properties:
+            self._properties[name].fset(value)
+        else:
+            raise AttributeError("mock object {!r} has no property {!r}".format(self._name, name))
 
     @property
     def __methods__(self):
         return self._methods
+
+    def expect_set(self, name, value):
+        self._properties[name].fset.expect_call(value)
 
     def assert_satisfied(self):
         self._registry.assert_satisfied()
--- tests/mock/test_object.py
+++ tests/mock/test_object.py
@@ -8,7 +8,9 @@ from mockify.mock.function import Function
 class TestObject:
 
     def setup_method(self):
-        self.uut = Object('uut', methods=['foo', 'bar'])
+        self.uut = Object('uut', 
+                methods=['foo', 'bar'],
+                properties=['spam'])
 
     ### Tests
 
@@ -16,7 +18,7 @@ class TestObject:
         with pytest.raises(AttributeError) as excinfo:
             ret = self.uut.baz()
 
-        assert str(excinfo.value) == "'uut' mock object has no attribute named 'baz'"
+        assert str(excinfo.value) == "mock object 'uut' has no attribute named 'baz'"
 
     def test_when_invoking_a_method_with_unexpected_args__then_uninterested_call_exception_is_raised(self):
         with pytest.raises(exc.UninterestedCall) as excinfo:
@@ -35,3 +37,25 @@ class TestObject:
         self.uut.foo(1, 2)
         with pytest.raises(exc.Unsatisfied):
             self.uut.assert_satisfied()
+
+    def test_when_two_method_call_expectations_recorded_and_both_call__then_object_mock_is_satisfied(self):
+        self.uut.__methods__['foo'].expect_call()
+        self.uut.__methods__['bar'].expect_call()
+        self.uut.foo()
+        self.uut.bar()
+        self.uut.assert_satisfied()
+
+    def test_when_property_set_while_no_expectations_are_recorded__then_fail_with_uninterested_call(self):
+        with pytest.raises(exc.UninterestedCall):
+            self.uut.spam = 1
+
+    def test_when_setting_property_not_listed_in_properties_list__then_fail_with_attribute_error(self):
+        with pytest.raises(AttributeError) as excinfo:
+            self.uut.more_spam = 123
+
+        assert str(excinfo.value) == "mock object 'uut' has no property 'more_spam'"
+
+    def test_expect_property_to_be_set_once_and_set_it_once(self):
+        self.uut.expect_set('spam', 1)
+        self.uut.spam = 1
+        self.uut.assert_satisfied()
