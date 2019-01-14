594eaee1847264cb3832cbf41c61a0e3a2ff44cf
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2019-01-14 18:36:21 +0100

    add docstring

--- mockify/mock/object.py
+++ mockify/mock/object.py
@@ -3,6 +3,41 @@ from mockify.mock.function import Function
 
 
 class Object:
+    """Mock frontend made for mocking Python objects.
+
+    This class requires custom subclass to be created and have following
+    attributes defined:
+
+        __methods__
+            Containing list of method names
+
+        __properties__
+            Containing list of property names
+
+    For example, if you want to mock Python class that has methods ``foo`` and
+    ``bar``, and propety ``spam``, you would create following subclass::
+
+        >>> class Mock(Object):
+        ...     __methods__ = ['foo', 'bar']
+        ...     __properties__ = ['spam']
+
+    After that, class ``Mock`` can be instantiated like this::
+
+        >>> mock = Mock('mock')
+
+    And then injected to some unit under test.
+
+    :param name:
+        Name of this object mock instance
+
+    :param registry:
+        This parameter is optional.
+
+        If you omit it, new instance of :class:`mockify.engine.Registry` will
+        be created and used by this object mock. This is useful if you already
+        have registry instance that you want to share between many unrelated
+        mock frontends.
+    """
     __methods__ = None
     __properties__ = None
 
@@ -55,13 +90,40 @@ class Object:
             raise AttributeError("mock object {!r} has no property {!r}".format(self._name, name))
 
     def expect_call(self, _name_, *args, **kwargs):
+        """Record method call expectation.
+
+        This method requires one argument to be given - method name. All other
+        are simply forwarded to underlying
+        :class:`mockify.mock.function.Function` object.
+
+        :param _name_:
+            Method name
+        """
         return self._methods[_name_].expect_call(*args, **kwargs)
 
     def expect_set(self, name, value):
+        """Record property set expectation.
+
+        :param name:
+            Property name
+        :param value:
+            Property value that is expected to be set.
+        """
         return self._properties[name].fset.expect_call(value)
 
     def expect_get(self, name):
+        """Record property get expectation.
+
+        :param name:
+            Property name
+        """
         return self._properties[name].fget.expect_call()
 
     def assert_satisfied(self):
+        """Check if this mock object is satisfied.
+
+        This method checks (in one call) if all registered expectations (for
+        all methods and all properties) are satisfied. If not, then
+        :exc:`mockify.exc.Unsatisfied` exception is raised.
+        """
         self._registry.assert_satisfied()
