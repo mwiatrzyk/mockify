28882322746add03b46364435b0f757353387836
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2019-01-14 18:41:23 +0100

    update version and add copyright notice to newly created files

--- mockify/mock/object.py
+++ mockify/mock/object.py
@@ -1,3 +1,14 @@
+# ---------------------------------------------------------------------------
+# mockify/mock/object.py
+#
+# Copyright (C) 2018 - 2019 Maciej Wiatrzyk
+#
+# This file is part of Mockify library and is released under the terms of the
+# MIT license: http://opensource.org/licenses/mit-license.php.
+#
+# See LICENSE.txt for details.
+# ---------------------------------------------------------------------------
+
 from mockify.engine import Registry
 from mockify.mock.function import Function
 
