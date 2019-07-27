eccc0c2aa4959f94391f99dabcfb635343c5ccdb
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2019-07-27 10:33:00 +0200

    move assert_satisfied to library core and make helpers.py deprecated

--- mockify/_engine.py
+++ mockify/_engine.py
@@ -18,6 +18,8 @@ import itertools
 import traceback
 import collections
 
+from contextlib import contextmanager
+
 from mockify import exc, _utils
 from mockify.times import Exactly, AtLeast
 
@@ -27,6 +29,36 @@ def _wrap_expected_count(expected_count):
         return Exactly(expected_count)
     else:
         return expected_count
+
+
+@contextmanager
+def assert_satisfied(*subjects):
+    """Context manager for verifying multiple subjects at once.
+
+    Each passed subject must have ``assert_satisfied`` method defined, so it
+    can be used with :class:`mockify.mock.function.Function` or
+    :class:`mockify.engine.Registry` instances for example.
+
+    Basically, the role of this helper is to ensure that all subjects become
+    satisfied after leaving wrapped context. For example:
+
+        >>> from mockify.mock.function import Function
+        >>> foo = Function('foo')
+        >>> bar = Function('bar')
+        >>> foo.expect_call()
+        <mockify.Expectation: foo()>
+        >>> bar.expect_call().times(0)
+        <mockify.Expectation: bar()>
+        >>> with assert_satisfied(foo, bar):
+        ...     foo()
+
+    And that's it - you don't have to explicitly check if ``foo`` and ``bar``
+    are satisfied, because the helper will do it for you. And also it
+    emphasizes part of code that actually uses given mocks.
+    """
+    yield
+    for subject in subjects:
+        subject.assert_satisfied()
 
 
 class Call:
