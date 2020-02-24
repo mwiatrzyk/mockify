c6ff18d10e6d1f5ee71b7445eda724dd5dbb278a
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2020-02-24 23:20:37 +0100

    fix versioning scheme

--- setup.py
+++ setup.py
@@ -3,11 +3,19 @@ import setuptools
 with open("README.md", "r") as fd:
     long_description = fd.read()
 
+def version_scheme(version):
+    return str(version.tag)
+
+def local_scheme(version):
+    return "rc{}".format(version.distance)
+
 setuptools.setup(
     name="mockify",
     #version='.'.join(str(x) for x in mockify.__version__),
     use_scm_version={
-        'write_to': 'mockify/_version.py'
+        'write_to': 'mockify/_version.py',
+        'version_scheme': version_scheme,
+        'local_scheme': local_scheme
     },
     setup_requires=['setuptools_scm'],
     author="Maciej Wiatrzyk",
