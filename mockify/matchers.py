c5628960fd9773469489cfe610126e01e560c118
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2024-04-22 17:24:05 +0200

    chore: update copyright year

--- mockify/matchers.py
+++ mockify/matchers.py
@@ -1,7 +1,7 @@
 # ---------------------------------------------------------------------------
 # mockify/matchers.py
 #
-# Copyright (C) 2019 - 2021 Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
+# Copyright (C) 2019 - 2024 Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
 #
 # This file is part of Mockify library and is released under the terms of the
 # MIT license: http://opensource.org/licenses/mit-license.php.
